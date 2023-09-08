4a6dded3ca399aff28715c31520f2f8d48b8c7b2
Andrei <mirceanghel98@gmail.com>
2023-09-08 19:24:54 +0300

    Updated with commant

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello");
-        //this is a comment from   main branch
+        //this is a comment from   main branch.
     }
 }
