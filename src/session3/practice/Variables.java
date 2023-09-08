a9d606c5fcc77222f019c8df1934ffdac96750ab
Andrei <mirceanghel98@gmail.com>
2023-09-08 21:18:33 +0300

    Created session2 package

---
+++ src/session3/practice/Variables.java
@@ -0,0 +1,22 @@
+package session3.practice;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        int age; //declaration
+        age = 27; //initialization
+
+        int  myAge = 29;
+        System.out.println("I'm John, and I'm " + age + " years old.");
+    }
+
+    public static void practiceVariablesScope(){
+        int outerVar = 24;
+
+        if (outerVar > 10) {
+            int innerVal = 200;
+            System.out.println(innerVal + outerVar);
+        }
+        //System.out.println(innerVal); this leads to a compiler error due to the variable scope being just the if;
+    }
+}
