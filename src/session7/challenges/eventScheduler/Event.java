c35413f23f5516791dab8ddc7f0b415af9066496
rhacp <mirceanghel98@gmail.com>
2023-09-25 13:39:52 +0200

    Second Challenges

--- src/session7/challenges/eventScheduler/Event.java
+++ src/session7/challenges/eventScheduler/Event.java
@@ -1,5 +1,57 @@
 package session7.challenges.eventScheduler;
 
+import java.time.LocalDateTime;
+
 public class Event {
     //In progress
+    private String eventName;
+    private String eventDescription;
+    private LocalDateTime eventDateTime;
+    private LocalDateTime reminderDateTime;
+
+    public Event(String eventName, String eventDescription, LocalDateTime eventDateTime, LocalDateTime reminderDateTime) {
+        this.eventName = eventName;
+        this.eventDescription = eventDescription;
+        this.eventDateTime = eventDateTime;
+        this.reminderDateTime = reminderDateTime;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getEventDescription() {
+        return eventDescription;
+    }
+
+    public void setEventDescription(String eventDescription) {
+        this.eventDescription = eventDescription;
+    }
+
+    public LocalDateTime getEventDateTime() {
+        return eventDateTime;
+    }
+
+    public void setEventDateTime(LocalDateTime eventDateTime) {
+        this.eventDateTime = eventDateTime;
+    }
+
+    public LocalDateTime getReminderDateTime() {
+        return reminderDateTime;
+    }
+
+    public void setReminderDateTime(LocalDateTime reminderDateTime) {
+        this.reminderDateTime = reminderDateTime;
+    }
+
+    public String timeUntilEvent() {
+        //Returns the time remaining until the event.
+    }
+    public String timeUntilReminder() {
+        //Returns the time left until the reminder should notify the user.
+    }
 }
---
+++ src/session7/challenges/eventScheduler/Executable.java
@@ -0,0 +1,10 @@
+package session7.challenges.eventScheduler;
+
+import java.util.Scanner;
+
+public class Executable {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
