2c0bfdb6926ee5b08ef1f4cdaa0eba0a7f801043
rhacp <mirceanghel98@gmail.com>
2023-09-25 04:15:12 +0200

    Second Challenges

--- src/session7/challenges/Challenge11.java
+++ src/session7/challenges/Challenge11.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class Challenge11 {
 
     public static void main(String[] args) {
-        System.out.println("CHALLENGE 9");
+        System.out.println("CHALLENGE 11");
         System.out.println("---");
         System.out.println("");
 
