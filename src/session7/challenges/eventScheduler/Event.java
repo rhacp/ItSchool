e95c3789002911ce65fa77d9c7fa3a65bcb99447
Andrei <mirceanghel98@gmail.com>
2023-09-25 22:18:38 +0300

    First Challenge

--- src/session7/challenges/eventScheduler/Event.java
+++ src/session7/challenges/eventScheduler/Event.java
@@ -1,6 +1,9 @@
 package session7.challenges.eventScheduler;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 
 public class Event {
     //In progress
@@ -8,13 +11,14 @@ public class Event {
     private String eventDescription;
     private LocalDateTime eventDateTime;
     private LocalDateTime reminderDateTime;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    public Event(String eventName, String eventDescription, LocalDateTime eventDateTime, LocalDateTime reminderDateTime) {
-        this.eventName = eventName;
-        this.eventDescription = eventDescription;
-        this.eventDateTime = eventDateTime;
-        this.reminderDateTime = reminderDateTime;
-    }
+//    public Event(String eventName, String eventDescription, String eventDateTime, String reminderDateTime) {
+//        this.eventName = eventName;
+//        this.eventDescription = eventDescription;
+//        this.eventDateTime = this.eventDateTime.parse(eventDateTime, formatter);
+//        this.reminderDateTime = this.reminderDateTime.parse(reminderDateTime, formatter);
+//    }
 
     public String getEventName() {
         return eventName;
@@ -36,22 +40,88 @@ public class Event {
         return eventDateTime;
     }
 
-    public void setEventDateTime(LocalDateTime eventDateTime) {
-        this.eventDateTime = eventDateTime;
+    public void setEventDateTime(String eventDateTime) {
+        this.eventDateTime = this.eventDateTime.parse(eventDateTime, this.formatter);
     }
 
     public LocalDateTime getReminderDateTime() {
         return reminderDateTime;
     }
 
-    public void setReminderDateTime(LocalDateTime reminderDateTime) {
-        this.reminderDateTime = reminderDateTime;
+    public void setReminderDateTime(String reminderDateTime) {
+        this.reminderDateTime = this.reminderDateTime.parse(reminderDateTime, this.formatter);
     }
 
     public String timeUntilEvent() {
-        //Returns the time remaining until the event.
+        LocalDateTime currentTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        DateTimeFormatter formatterS = DateTimeFormatter.ofPattern("ss");
+        DateTimeFormatter formatterMin = DateTimeFormatter.ofPattern("mm");
+        DateTimeFormatter formatterH = DateTimeFormatter.ofPattern("HH");
+        DateTimeFormatter formatterD = DateTimeFormatter.ofPattern("dd");
+        DateTimeFormatter formatterM = DateTimeFormatter.ofPattern("MM");
+        DateTimeFormatter formatterY = DateTimeFormatter.ofPattern("yyyy");
+
+        String rez1 = currentTime.format(formatterS);
+        LocalDateTime rez = this.eventDateTime.minusSeconds(Long.valueOf(rez1));
+
+        rez1 = currentTime.format(formatterMin);
+        rez = rez.minusMinutes(Long.valueOf(rez1));
+
+        rez1 = currentTime.format(formatterH);
+        rez = rez.minusHours(Long.valueOf(rez1));
+
+        rez1 = currentTime.format(formatterD);
+        rez = rez.minusDays(Long.valueOf(rez1));
+
+        rez1 = currentTime.format(formatterM);
+        rez = rez.minusMonths(Long.valueOf(rez1));
+
+        rez1 = currentTime.format(formatterY);
+        rez = rez.minusYears(Long.valueOf(rez1));
+
+        if (rez.getYear() == 0) {
+            return ("Time left until event: " + rez.format(formatterS) + " seconds, " + rez.format(formatterMin) + " minutes, " + rez.format(formatterH) + " hours, " + rez.format(formatterD) + " days, " + rez.format(formatterM) + " months, " + "0000 years.");
+
+        } else {
+            return ("Time left until event: " + rez.format(formatterS) + " seconds, " + rez.format(formatterMin) + " minutes, " + rez.format(formatterH) + " hours, " + rez.format(formatterD) + " days, " + rez.format(formatterM) + " months, " + rez.format(formatterY) + " years.");
+        }
     }
+
     public String timeUntilReminder() {
         //Returns the time left until the reminder should notify the user.
+        LocalDateTime currentTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        DateTimeFormatter formatterS = DateTimeFormatter.ofPattern("ss");
+        DateTimeFormatter formatterMin = DateTimeFormatter.ofPattern("mm");
+        DateTimeFormatter formatterH = DateTimeFormatter.ofPattern("HH");
+        DateTimeFormatter formatterD = DateTimeFormatter.ofPattern("dd");
+        DateTimeFormatter formatterM = DateTimeFormatter.ofPattern("MM");
+        DateTimeFormatter formatterY = DateTimeFormatter.ofPattern("yyyy");
+
+        String rez1 = currentTime.format(formatterS);
+        LocalDateTime rez = this.reminderDateTime.minusSeconds(Long.valueOf(rez1));
+
+        rez1 = currentTime.format(formatterMin);
+        rez = rez.minusMinutes(Long.valueOf(rez1));
+
+        rez1 = currentTime.format(formatterH);
+        rez = rez.minusHours(Long.valueOf(rez1));
+
+        rez1 = currentTime.format(formatterD);
+        rez = rez.minusDays(Long.valueOf(rez1));
+
+        rez1 = currentTime.format(formatterM);
+        rez = rez.minusMonths(Long.valueOf(rez1));
+
+        rez1 = currentTime.format(formatterY);
+        rez = rez.minusYears(Long.valueOf(rez1));
+
+        if (rez.getYear() == 0) {
+            return ("Time left until event: " + rez.format(formatterS) + " seconds, " + rez.format(formatterMin) + " minutes, " + rez.format(formatterH) + " hours, " + rez.format(formatterD) + " days, " + rez.format(formatterM) + " months, " + "0000 years.");
+
+        } else {
+            return ("Time left until event: " + rez.format(formatterS) + " seconds, " + rez.format(formatterMin) + " minutes, " + rez.format(formatterH) + " hours, " + rez.format(formatterD) + " days, " + rez.format(formatterM) + " months, " + rez.format(formatterY) + " years.");
+        }
     }
 }
--- src/session7/challenges/eventScheduler/Executable.java
+++ src/session7/challenges/eventScheduler/Executable.java
@@ -5,6 +5,65 @@ import java.util.Scanner;
 public class Executable {
 
     public static void main(String[] args) {
+//        Event wedding = new Event("Wedding", "Just a wedding!", "2024-02-22 20:00:00", "2024-01-22 20:00:00");
+//
+//        System.out.println(wedding.timeUntilEvent());
+//        System.out.println(wedding.timeUntilReminder());
         Scanner scanner = new Scanner(System.in);
+        Event event2 = new Event();
+        event2.setEventName(scanner.nextLine());
+        menu();
+    }
+
+
+    public static void menu() {
+        Scanner scanner = new Scanner(System.in);
+        int helpCounter, secondCounter;
+        System.out.println("FIRST MENU");
+        System.out.println("Choose your item:");
+        System.out.println("1. Add new event and select it.");
+        System.out.println("2. Get events list and select one.");
+        System.out.println("3. Remove event.");
+        System.out.print("Choose the item by number: ");
+        helpCounter = scanner.nextInt();
+        System.out.println("");
+
+        switch (helpCounter) {
+            case 1 -> {
+                Event event1 = new Event();
+                System.out.print("Enter event name: ");
+                event1.setEventName(scanner.nextLine());
+                System.out.println("");
+
+                System.out.print("Enter event description: ");
+                event1.setEventDescription(scanner.next());
+                System.out.println("");
+
+                System.out.print("Enter event date and time (yyyy-MM-dd HH:mm:ss): ");
+                event1.setEventDateTime(scanner.nextLine());
+                System.out.println("");
+
+                System.out.print("Enter reminder date and time (yyyy-MM-dd HH:mm:ss): ");
+                event1.setReminderDateTime(scanner.nextLine());
+                System.out.println("");
+
+                System.out.println("SECOND MENU");
+                System.out.println("Choose your item:");
+                System.out.println("1. Get time until the selected event.");
+                System.out.println("2. Get time until the selected event reminder.");
+                System.out.print("Choose the item by number: ");
+                secondCounter = scanner.nextInt();
+
+                switch (secondCounter) {
+                    case 1 -> {
+                        System.out.println(event1.timeUntilEvent());
+                    }
+                    case 2 -> {
+                        System.out.println(event1.timeUntilReminder());
+                    }
+                }
+            }
+        }
+        scanner.close();
     }
 }
