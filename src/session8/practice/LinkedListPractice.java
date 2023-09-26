6e50f1472a031a6a4b71dfc19dae0c2a9cbac0d9
rhacp <mirceanghel98@gmail.com>
2023-09-26 19:40:40 +0200

    Second Challenges

---
+++ src/session8/practice/LinkedListPractice.java
@@ -0,0 +1,11 @@
+package session8.practice;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListPractice {
+
+    public static void main(String[] args) {
+        List<String> linkedList = new LinkedList<>();
+    }
+}
