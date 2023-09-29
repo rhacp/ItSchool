df6888ecae75bc73d711b3cb800d29b5456fc205
Andrei <mirceanghel98@gmail.com>
2023-09-29 20:00:25 +0300

    codingKata recover

---
+++ src/session1/Homework.java
@@ -0,0 +1,11 @@
+public class Homework {
+
+    public static void main(String[] args) {
+        System.out.println("NUMBER 1");
+        System.out.println("Name: Andrei");
+        System.out.println("Age: 25");
+        System.out.println("Favorite Color: Purple");
+        //ala bala
+    }
+}
+
