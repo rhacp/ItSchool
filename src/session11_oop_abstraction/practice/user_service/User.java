ec53db7cd1a491b231608c6c8ba7b6616b838266
Andrei <mirceanghel98@gmail.com>
2023-10-08 07:37:26 +0300

    Done

--- src/session11_oop_abstraction/practice/user_service/User.java
+++ src/session11_oop_abstraction/practice/user_service/User.java
@@ -1,4 +1,23 @@
 package session11_oop_abstraction.practice.user_service;
 
 public class User {
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
