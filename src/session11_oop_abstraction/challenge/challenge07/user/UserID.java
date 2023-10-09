41733f4b5c3ae15d30074f1c43023683b7f27aec
Andrei <mirceanghel98@gmail.com>
2023-10-09 13:09:02 +0300

    Structure done, still needs actual implementation.

--- src/session11_oop_abstraction/challenge/challenge07/user/UserID.java
+++ src/session11_oop_abstraction/challenge/challenge07/user/UserID.java
@@ -2,5 +2,10 @@ package session11_oop_abstraction.challenge.challenge07.user;
 
 public interface UserID {
 
+    /***
+     * In Java , interface doesn't allow you to declare any instance variables.
+     * Using a variable declared in an interface as an instance variable will return a compile time error.
+     * You can declare a constant variable, using static final which is different from an instance variable.
+     */
     void setUserId();
 }
