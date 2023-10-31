43b79fc298baec1102c79fffcab74d4329b60cd0
Andrei <mirceanghel98@gmail.com>
2023-10-31 15:17:01 +0200

    Session 17 - Challenges In Progress

--- src/session17_streams_optional_method_reference/challenge/Challenge10.java
+++ src/session17_streams_optional_method_reference/challenge/Challenge10.java
@@ -1,10 +1,21 @@
 package session17_streams_optional_method_reference.challenge;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 public class Challenge10 {
 
     public static void main(String[] args) {
-
+        List<String> strings = Arrays.asList("Java Streams", "Method References", "Lambda Expressions");
+        printNumberOfCharacters(strings);
     }
 
-
+    private static void printNumberOfCharacters(List<String> stringList) {
+        int result = stringList.stream()
+                .map(string -> string.replace(" ", ""))
+                .map(String::length)
+                .reduce(0, (firstNumber, secondNumber) -> firstNumber + secondNumber);
+        System.out.println("Number of characters in list: " + result);
+    }
 }
---
+++ src/session17_streams_optional_method_reference/challenge/Challenge13.java
@@ -0,0 +1,10 @@
+package session17_streams_optional_method_reference.challenge;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Challenge13 {
+
+    public static void main(String[] args) {
+    }
+}
